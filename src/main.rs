9330d908a5a31d47e3e53426b896c560beac013b
slme9364 <ishikazu1104@yahoo.co.jp>
2017-02-24 15:47:48 +0900

    [Fix] fixed comment

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,21 @@ use std::net::SocketAddrV4;
 use std::net::Ipv4Addr;
 use std::str;
 
-// if "hostname -I" responese more than two or no Ipv4Addres, this method use.
+// if "hostname -I" responese two, more or no Ipv4Addres, this method use.
 fn set_local_addres(host_port: u16) -> SocketAddrV4 {
     let ip = Ipv4Addr::new(127, 0, 0, 1);
     SocketAddrV4::new(ip, host_port)
 }
 
 fn get_addres() -> SocketAddrV4 {
-    let host_port = 8080;
+    let host_port = 8080;//GAE needs 8080 ports.
 
     // "hostname -I" -> get the host ip addres
     let hostname_cmd = Command::new("hostname").arg("-I").output();
     let host_addres: SocketAddrV4 = match hostname_cmd {
 
         Ok(res) => {
-            // host ip addres -> addr
+            // host ip addres -> addres
             let addres = str::from_utf8(res.stdout.as_slice())
                 .map_err(|err| err.to_string())
                 .and_then(|ip_str| {
